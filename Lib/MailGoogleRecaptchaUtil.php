c4663e9f89bec1144f61e036708cdc0f3187f9a9
arata <contact@materializing.net>
2019-12-31 13:33:58 +0900

    clean コメント記載

--- Lib/MailGoogleRecaptchaUtil.php
+++ Lib/MailGoogleRecaptchaUtil.php
@@ -9,6 +9,12 @@
  */
 class MailGoogleRecaptchaUtil {
 
+	/**
+	 * 利用設定が済んでいるか確認する
+	 *
+	 * @param array $configData
+	 * @return boolean
+	 */
 	public static function hasUseConfig($configData = []) {
 		if (!$configData) {
 			$configData = self::getUseConfig();
@@ -22,12 +28,22 @@ class MailGoogleRecaptchaUtil {
 		return false;
 	}
 
+	/**
+	 * フォーム別設定の一覧を取得する
+	 *
+	 * @return array
+	 */
 	public static function getSettingList() {
 		$SiteConfigModel = ClassRegistry::init('SiteConfig');
 		$data = $SiteConfigModel->findByName(Configure::read('MailGoogleRecaptcha.key_name'));
 		return json_decode($data['SiteConfig']['value'], true);
 	}
 
+	/**
+	 * 利用設定の値を取得する
+	 *
+	 * @return array
+	 */
 	public static function getUseConfig() {
 		$SiteConfigModel = ClassRegistry::init('SiteConfig');
 		$data = $SiteConfigModel->findByName(Configure::read('MailGoogleRecaptcha.config_key_name'));
